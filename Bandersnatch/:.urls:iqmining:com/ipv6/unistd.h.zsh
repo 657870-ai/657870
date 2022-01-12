3669c7a932dc1a47b18a96b3ef1cdb43298d6534
Andres Barbudo Rodriguez <47259327+AndresBarbudoRodriguez@users.noreply.github.com>
2022-01-12 15:20:57 -0300

    1.1 sanitización de entorno, las variables de entorno obtenidas y establecidas por usuario o moderador permanecen restringidas durante la revisión de protocolos de seguridad

--- Bandersnatch/:.urls:iqmining:com/ipv6/unistd.h.zsh
+++ Bandersnatch/:.urls:iqmining:com/ipv6/unistd.h.zsh
@@ -17,4 +17,50 @@ r SHELL getenv
         GLOB_SUBST
         NO_GLOBAL_EXPORT
         NO_HUP
+        INTERACTIVE_COMMENTS
+        KSH_ARRAYS
+        NU_MULTIOS
+        NO_NOMATCH
+        NO_NOTIFY
+        POSIX_BUILTINS
+        NO_PROMPT_PERCENT
+        RM_STAR_SILENT
+        SH_FILE_EXPANSION
+        SH_GLOB
+        SH_OPTION_LETTERS
+        SH_WORD_SPLIT
+        BSD_ECHO=off
+        IGNORE_BRACES
+        KSH_OPTION_PRINT
+        LOCAL_OPTIONS
+        PROMPT_BANG
+        PROMPT_SUBST
+        SINGLE_LINE_ZLE
+}
+
+"Restricted Shell"(-r){
+  PATH
+  path
+  MODULE_PATH
+  module_path
+  SHELL
+  HISTFILE
+  HISTSIZE
+  GID
+  EGID
+  UID
+  EUID
+  USERNAME
+  LD_LIBRARY_PATH
+  LD_AOUT_LIBRARY_PATH
+  LD_PRELOAD
+  LD_AOUT_PRELOAD
+  /
+  hash
+  |
+  exec
+  jobs-Z
+  ARGV0
+  set +r
+  unsetopt RESTRICTED
 }
