497a16be4cf0d65e8c1a26f0c290b068cee60bec
Andres Barbudo Rodriguez <47259327+AndresBarbudoRodriguez@users.noreply.github.com>
2022-01-12 15:11:30 -0300

    sanitización de entorno, las variables de entorno obtenidas y establecidas por usuario o moderador permanecen restringidas durante la revisión de protocolos de seguridad

---
+++ Bandersnatch/:.urls:iqmining:com/ipv6/unistd.h.zsh
@@ -0,0 +1,20 @@
+//
+// Created by Andres Barbudo Rodriguez on 12-01-22.
+//
+
+extern char **environ
+
+#include <stdlib.h>
+
+r SHELL getenv
+        putenv (/.zsh) {
+
+    $ENV
+        NO_BANG_HIST
+        NO_BG_NICE
+        NO_EQUALS
+        NO_FUNCTION_ARGZERO
+        GLOB_SUBST
+        NO_GLOBAL_EXPORT
+        NO_HUP
+}
